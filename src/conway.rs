b26a19d643397b9b5b8dd6636fe1a359a02243b5
Cyril Marinos <cyrilmarinos@gmail.com>
2021-10-29 13:44:17 -0700

    Added graphics animation to my cute boyfriend's Rust program ❤️

--- src/conway.rs
+++ src/conway.rs
@@ -1,7 +1,7 @@
 use std::{cell::RefCell, fmt};
 use rand::Rng;
 
-pub fn calc_idx_periodic(hor_idx: u32, vert_idx: u32, 
+pub fn calc_idx_periodic(hor_idx: u32, vert_idx: u32,
                      width: u32, height: u32) -> usize {
     ((hor_idx % width) + width * (vert_idx % height)) as usize
 }
@@ -10,8 +10,8 @@ pub fn calc_idx_periodic(hor_idx: u32, vert_idx: u32,
 pub struct Grid{
     // RefCell to get mutability in a generally
     // immutable attribute
-    refstate: RefCell<Vec<u8>>,
-    refstep: RefCell<u32>,
+    pub refstate: RefCell<Vec<u8>>,
+    pub refstep: RefCell<u32>,
     pub width: u32,
     pub height: u32,
 }
@@ -27,15 +27,15 @@ impl Grid{
         let state: Vec<u8> = (0..tot_nodes).map(|_| {
             rng.gen_range(0, 2) // 0 included, 2 excluded
         }).collect();*/
-        
-        
+
+
         // imperative approach
-        let mut state: Vec<u8> = 
+        let mut state: Vec<u8> =
             Vec::with_capacity(tot_nodes as usize);
         for _ in 0..tot_nodes {
             state.push(rng.gen_range(0, 2));
         }
-        
+
         /*
         // procedural approach
         let mut state: Vec<u8> = vec![0; tot_nodes as usize];
@@ -44,7 +44,7 @@ impl Grid{
             *i = rng.gen_range(0, 2);
         }
         */
-        
+
         Grid {
             width,
             height,
@@ -52,9 +52,9 @@ impl Grid{
             refstep: RefCell::new(0),
         }
     }
-    
+
     pub fn new_like(other_grid: &Grid) -> Grid {
-        let tot_nodes: u32 = other_grid.width * 
+        let tot_nodes: u32 = other_grid.width *
                                 other_grid.height;
         let mut rng = rand::thread_rng();
         // functional approach
@@ -67,27 +67,27 @@ impl Grid{
             ..*other_grid
         }
     }
-    
+
     pub fn reset(&self) {
         let mut step = self.refstep.borrow_mut();
         *step = 0;
-        
+
         let mut state = self.refstate.borrow_mut();
         let mut rng = rand::thread_rng();
         for i in state.iter_mut() {
             *i = rng.gen_range(0, 2);
         }
     }
-    
+
     pub fn evolve(&self) {
         let mut step = self.refstep.borrow_mut();
         *step += 1;
-        
+
         let mut state = self.refstate.borrow_mut();
         let state_copy = state.clone();
         for i in 0..self.height {
             for j in 0..self.width{
-                let idx = calc_idx_periodic(j, i, 
+                let idx = calc_idx_periodic(j, i,
                                             self.width,
                                             self.height
                                            );
@@ -116,13 +116,13 @@ impl Grid{
             }
         }
     }
-    
+
     pub fn print_state(&self) {
         let state = self.refstate.borrow();
         let mut row: Vec<u8> = vec![0; self.width as usize];
         for i in 0..self.height {
             for (j, col) in row.iter_mut().enumerate() {
-                let idx = calc_idx_periodic(j as u32, i, 
+                let idx = calc_idx_periodic(j as u32, i,
                                             self.width,
                                             self.height
                                            );
@@ -131,7 +131,7 @@ impl Grid{
             println!("{:?}", row);
         }
     }
-    
+
     pub fn set_state(&self, new_state: &[u8]) {
         let len_new = new_state.len();
         let tot_nodes = (self.width * self.height) as usize;
@@ -140,17 +140,17 @@ impl Grid{
             "The size of the input state is not the same as the width times height: {}.", tot_nodes as u32
             );
         }
-        
+
         for el in new_state {
             if *el > 1 {
                 panic!("Valid values can only be 0 or 1. Got value of {}.", *el);
             }
         }
-        
+
         let mut state = self.refstate.borrow_mut();
         *state = new_state.to_vec();
     }
-    
+
     pub fn eat_state(&self, new_state: Vec<u8>) {
         let len_new = new_state.len();
         let tot_nodes = (self.width * self.height) as usize;
@@ -159,13 +159,13 @@ impl Grid{
             "The size of the input state is not the same as the width times height: {}.", tot_nodes as u32
             );
         }
-        
+
         for el in &new_state {
             if *el > 1 {
                 panic!("Valid values can only be 0 or 1. Got value of {}.", *el);
             }
         }
-        
+
         let mut state = self.refstate.borrow_mut();
         *state = new_state;
     }
@@ -175,24 +175,24 @@ impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // lucky me returning Result can use ? operator
         let step = self.refstep.borrow();
-        write!(f, 
-               "Grid type: {{\n\n    width: {}, height: {}, step: {}\n\n", 
+        write!(f,
+               "Grid type: {{\n\n    width: {}, height: {}, step: {}\n\n",
                self.width, self.height, step)?;
         let state = self.refstate.borrow();
         let mut row: Vec<u8> = vec![0; self.width as usize];
         for i in 0..self.height {
             if i == 0 {
-                write!(f, "┏━━━{}┓\n", 
+                write!(f, "┏━━━{}┓\n",
                     &"┳━━━".repeat((self.width - 1) as usize)[..]
                     )?;
             } else {
-                write!(f, "┣━━━{}┫\n", 
+                write!(f, "┣━━━{}┫\n",
                     &"╋━━━".repeat((self.width - 1) as usize)[..]
                     )?;
                 //write!(f, "[")?;
             }
             for (j, col) in row.iter_mut().enumerate() {
-                let idx = calc_idx_periodic(j as u32, i, 
+                let idx = calc_idx_periodic(j as u32, i,
                                             self.width,
                                             self.height
                                            );
@@ -204,11 +204,11 @@ impl fmt::Display for Grid {
                 }
             }
             write!(f, "┃\n")?;
-            
+
         }
-        write!(f, "┗━━━{}┛\n\n}}\n", 
+        write!(f, "┗━━━{}┛\n\n}}\n",
             &"┻━━━".repeat((self.width - 1) as usize)[..])
-        
+
     }
 }
 
@@ -226,13 +226,13 @@ mod tests {
                            0, 1, 1, 0, 0,
                            0, 0, 0, 0, 0];
         sim.eat_state(state_0);
-        
+
         let state_1 = vec![0, 0, 0, 0, 0,
                            0, 0, 1, 0, 0,
                            1, 1, 0, 0, 0,
                            0, 1, 1, 0, 0,
                            0, 0, 0, 0, 0];
-        
+
         let state_2 = vec![0, 0, 0, 0, 0,
                            0, 1, 0, 0, 0,
                            1, 0, 0, 0, 0,
@@ -244,13 +244,13 @@ mod tests {
                            1, 0, 1, 0, 0,
                            1, 1, 0, 0, 0,
                            0, 1, 0, 0, 0];
-        
+
         let state_4 = vec![0, 0, 0, 0, 0,
                            0, 0, 0, 0, 0,
                            1, 0, 0, 0, 0,
                            1, 0, 1, 0, 0,
                            1, 1, 0, 0, 0];
-        
+
         sim.evolve();
         assert_eq!(*sim.refstate.borrow(), state_1);
         sim.evolve();
@@ -260,7 +260,7 @@ mod tests {
         sim.evolve();
         assert_eq!(*sim.refstate.borrow(), state_4);
     }
-    
+
     #[test]
     fn test_set_state() {
         let sim = Grid::new(5, 5);
@@ -270,10 +270,10 @@ mod tests {
                            0, 1, 1, 0, 0,
                            0, 0, 0, 0, 0];
         sim.set_state(&state_0);
-        
+
         assert_eq!(*sim.refstate.borrow(), state_0);
     }
-    
+
     #[test]
     #[should_panic(expected = "The size of the input state is not the same as the width times height: ")]
     fn test_set_state_incorrect_size() {
@@ -285,7 +285,7 @@ mod tests {
                            0, 0, 0, 0];
         sim.set_state(&state_0);
     }
-    
+
     #[test]
     #[should_panic(expected = "Valid values can only be 0 or 1. Got value of ")]
     fn test_set_state_invalid_state() {
@@ -297,19 +297,19 @@ mod tests {
                            0, 0, 0, 0, 1];
         sim.set_state(&state_0);
     }
-    
+
     #[test]
     #[should_panic]
     fn test_new_zero_size_width() {
         let _sim = Grid::new(0, 5);
     }
-    
+
     #[test]
     #[should_panic]
     fn test_new_zero_size_height() {
         let _sim = Grid::new(5, 0);
     }
-    
+
     #[test]
     #[should_panic]
     fn test_new_zero_size_width_and_height() {
